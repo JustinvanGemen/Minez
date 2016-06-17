1de10fb0dea7c881b7b2a0e5b990f027fdeca0f4
Justin van Gemen <20277@ma-web.nl>
2016-06-17 09:08:44 +0100

    duptate

--- Assets/_scripts/PickUps.cs
+++ Assets/_scripts/PickUps.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class PickUps : MonoBehaviour {
     [HideInInspector]
-	public PlayerShooting ps;
+
+    public Vector3 rotation;
+    public float rotationSpeed;
+    //public PlayerShooting ps;
+
 	void Start(){
 			//ps = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerShooting> ();
     }
 	void Update () {
-		
-		transform.Rotate (new Vector3 (0, 60, 60) * Time.deltaTime);
-	}
+
+       
+        transform.Rotate (new Vector3 (60, 0, 60) * Time.deltaTime);
+    }
 
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
